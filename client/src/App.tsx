1ed43a95885725d70daddf281a2d0d9b105e0c37
okmttdhr <10101114fin@gmail.com>
2020-03-06 00:14:59 +0900

    Remove logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { GreeterClient } from './HelloworldServiceClientPb';
 import { HelloRequest } from './helloworld_pb';
@@ -16,7 +15,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
